c7baa97aad44dbd2e82bf768c62fc3eef2785bec
Artem Klopov <artemklopow@gmail.com>
2018-04-05 19:20:14 +0300

    cheeki

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf;
-sudo rm -rf /etc/nginx/sites-enabled/default;
+sudo ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf;
 sudo nginx -t;
 sudo /etc/init.d/nginx restart;

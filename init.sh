323421f37037d57ac02d36bb5a2c73d38e504e32
Artem Klopov <artemklopow@gmail.com>
2018-04-05 19:22:29 +0300

    breeki

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf;
+sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default;
 sudo nginx -t;
 sudo /etc/init.d/nginx restart;

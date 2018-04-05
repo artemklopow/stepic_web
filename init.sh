2db40626ba72db022ecea23cd99fb99aac0fa214
Artem Klopov <artemklopow@gmail.com>
2018-04-05 21:42:10 +0300

    v damki

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo ln -sf home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo nginx -t
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart

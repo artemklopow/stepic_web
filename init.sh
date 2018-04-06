19a2f67f6d1cf8713bcbbd642fa099cda95e29ed
Artem Klopov <artemklopow@gmail.com>
2018-04-07 01:24:39 +0300

    wsgi

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 mkdir etc
-cp home/box/web/etc/hello.py /home/box/etc/hello.py
+cp /home/box/web/etc/hello.py /home/box/etc/hello.py
 gunicorn -c etc/hello.py web/hello:application

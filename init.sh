6a603ce7c4aeea10d0cdcfb3fb68942e2114e4af
Artem Klopov <artemklopow@gmail.com>
2018-04-07 02:23:40 +0300

    again

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
-mkdir etc
-cp /home/box/web/etc/hello.py /home/box/etc/hello.py
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicort restart
-gunicorn -c etc/hello.py hello:application &
+gunicorn -c /etc/gunicorn.d/hello.py hello:application &

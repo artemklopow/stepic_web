a4281b3e6cc2cbe4bdb84e11f23b9a0cdd2d4aca
Artem Klopov <artemklopow@gmail.com>
2018-04-07 02:42:49 +0300

    again

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-sudo -c /etc/gunicorn.d/hello.py hello:application
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application

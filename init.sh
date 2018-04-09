5c85852f4f2ff2b17567b71517b69916398d0c57
Artem Klopov <artemklopow@gmail.com>
2018-04-09 17:16:24 +0300

    /

--- init.sh
+++ init.sh
@@ -4,9 +4,7 @@ sudo cp /home/box/web/etc/gunicorn-debian /usr/sbin/gunicorn-debian
 sudo cp /home/box/web/etc/gunicorn /usr/bin/gunicorn
 sudo cp /home/box/web/etc/gunicorn_django /usr/bin/gunicorn_django
 sudo cp /home/box/web/etc/gunicorn_paster /usr/bin/gunicorn_paster
-
-
-
+source web/venv/bin/activate
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart

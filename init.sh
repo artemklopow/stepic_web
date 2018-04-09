6a77df1cf65f4bbb9149749ceea7c2f971b1b1c8
Artem Klopov <artemklopow@gmail.com>
2018-04-09 15:08:25 +0300

    ...

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
+sudo pip3 install --upgrade django
+sudo ln -sf python3 /usr/bin/python
+sudo cp /home/box/web/etc/gunicorn-debian /usr/sbin/gunicorn-debian
+sudo cp /home/box/web/etc/gunicorn /usr/bin/gunicorn
+sudo cp /home/box/web/etc/gunicorn_django /usr/bin/gunicorn_django
+sudo cp /home/box/web/etc/gunicorn_paster /usr/bin/gunicorn_paster
+
+
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart

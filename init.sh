e3f89e407a0591567100f0d745c762915439fed8
Artem Klopov <artemklopow@gmail.com>
2018-04-09 21:43:08 +0300

    djng_1

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@ sudo cp /home/box/web/etc/gunicorn-debian /usr/sbin/gunicorn-debian
 sudo cp /home/box/web/etc/gunicorn /usr/bin/gunicorn
 sudo cp /home/box/web/etc/gunicorn_django /usr/bin/gunicorn_django
 sudo cp /home/box/web/etc/gunicorn_paster /usr/bin/gunicorn_paster
-source web/ask/venv/bin/activate
+source /home/box/web/ask/venv/bin/activate
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 cd /home/box/web/ask
-sudo gunicorn -c /home/box/web/ask/ask/ask_gunicorn_config wsgi:application
+sudo gunicorn -c /home/box/web/ask/ask/ask_wsgi_config ask.wsgi:application

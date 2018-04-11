86391b025b6bbf850bb2dc75bcddf5daaafa2a89
Artem Klopov <artemklopow@gmail.com>
2018-04-11 22:50:01 +0300

    paginate

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -10,6 +10,12 @@ class QuestionManager(models.Manager):
         return self.ordered('-rating')
 
 
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.OneToOneField(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
@@ -17,16 +23,8 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='likes')
-
     objects = QuestionManager()
 
     def get_url(self):
         url = '/question/' + str(self.id) + '/'
         return url
-
-
-class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    question = models.OneToOneField(Question, null=True, on_delete=models.SET_NULL)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
